6afdad7f7f58fed672e1e0d2addac59a14c18751
Zykli <ZykliZen@gmail.com>
2017-03-29 11:21:32 +0300

    Update index.php

--- dz1-5/index.php
+++ dz1-5/index.php
@@ -1,42 +1,33 @@
-<?php
-  $content = file_get_contents('phonebook.json');
-  $phoneBook = json_decode($content, true);
-  $firstName = $phoneBook[0]['firstName'];
-  $lastName = $phoneBook[0]['lastName'];
-  echo "<div style='position: relative;'>";
-  echo "<h2>Телефоннкая книга</h2>";
-    echo "<table>";
-      echo "<thead>";
-        echo "<td>";
-          echo "Имя";
-        echo "</td>";
-        echo "<td>";
-          echo "Фамилия";
-        echo "</td>";
-        echo "<td>";
-          echo "Адресс";
-        echo "</td>";
-        echo "<td>";
-          echo "Номер телефона";
-        echo "</td>";
-      echo "</thead>";
-      echo "<tbody>";
-      for ($i=0; $i < count($phoneBook) ; $i++) {
-        echo "<tr>";
-          echo "<td>";
-            echo $phoneBook[$i]['firstName'];
-          echo "</td>";
-          echo "<td>";
-            echo $phoneBook[$i]['lastName'];
-          echo "</td>";
-          echo "<td>";
-            echo $phoneBook[$i]['address'];
-          echo "</td>";
-          echo "<td>";
-            echo $phoneBook[$i]['phoneNumber'];
-          echo "</td>";
-        echo "</tr>";
-      }
-      echo "</tbody>";
-    echo "</table>";
-  echo "</div>";
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="master.css" />
+</head>
+<body>
+	<div>
+		<h2></h2>
+		<table>
+			<thead>
+				<tr>
+					<th>Имя</th>
+					<th>Фамилия</th>
+					<th>Адресс</th>
+					<th>Номер телефона</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+				$content = file_get_contents('phonebook.json');
+				$phoneBook = json_decode($content, true);
+				for ($i=0; $i < count($phoneBook) ; $i++):?>
+				    <tr>
+			        	<td><?=$phoneBook[$i]['firstName']?></td>
+			        	<td><?=$phoneBook[$i]['lastName']?></td>
+			        	<td><?=$phoneBook[$i]['address']?></td>
+			        	<td><?=$phoneBook[$i]['phoneNumber']?></td>
+			        </tr>
+			    <?php endfor;?>
+			</tbody>
+		</table>
+	</div>
+</body>
