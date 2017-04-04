a90d2af6cdc702a99006675a26ccb0860fd34991
Zykli <ZykliZen@gmail.com>
2017-04-04 23:12:23 +0300

    Add files via upload

---
+++ 2-4/sertificate.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once 'functions.php';
+if(!isLogged()) {
+	header('Location: http://university.netology.ru/u/zenkin/PHP/2-4/index.php');
+	die;
+}
+	header('Content-type: image/png');
+
+	if (getLoggedUser()['name'] == 'guest') {
+		$userName = isset($_GET['name']) ? $_GET['name'] : 'Иванов Иван';
+	} else {
+		$userName = (string)getLoggedUser()['name'];
+	}
+
+	renderImg($userName);
